dec198861c80327f75b6e115439030c6fb3fd4cd
IsaacShure <shureisaac@gmail.com>
2020-07-23 22:21:57 -0400

    read from csv and organize data

--- exoplanets.py
+++ exoplanets.py
@@ -1,6 +1,36 @@
 """
-Spyder Editor
+exoplanets.py
 
-This is a temporary script file.
+by Isaac Shure
 """
 import pandas as pd
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+
+
+import matplotlib.pyplot as plt
+plt.close('all')
+trainset = pd.read_csv('exotrain.csv')
+
+"""
+t = range(0, 3198)
+fig, ax = plt.subplots()
+ax.plot(t, trainset.iloc[1], label="row 1")
+ax.plot(t, trainset.iloc[2], label="row 2")
+ax.plot(t, trainset.iloc[3], label="row 3")
+ax.legend()
+
+five = trainset[:5]
+"""
+
+# split dataframe by label (excluding label column)
+label1 = trainset.loc[trainset['LABEL'] == 1].drop(['LABEL'], axis=1)
+label2 = trainset.loc[trainset['LABEL'] == 2].drop(['LABEL'], axis=1)
+
+#model = Sequential()
+#model.add(Embedding())
